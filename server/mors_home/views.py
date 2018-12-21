cbc8edacc0175616441ab5933d0fe1b539141a55
mwoss <wos.mateusz16@gmail.com>
2018-12-21 02:43:20 +0100

    Remove comments on code

--- server/mors_home/views.py
+++ server/mors_home/views.py
@@ -10,23 +10,22 @@ from search_engine.engine.engines.tfidf_engine import TfidfEngine
 
 from server.mors_home.model import Site
 
-# aggregator = AggregateSearch(LdaEngine.from_configfile(), TfidfEngine.from_configfile(),
-#                              D2VEngine.from_configfile())
+aggregator = AggregateSearch(LdaEngine.from_configfile(), TfidfEngine.from_configfile(),
+                             D2VEngine.from_configfile())
 
 
 @api_view(['GET'])
 @permission_classes((AllowAny,))
 def search(request: Request):
-    pass
-    # query = request.query_params.get('query')
-    # limit = request.query_params.get('limit', 15) % 50
-    # result = aggregator.search(query)[:limit]
-    #
-    # return Response({
-    #     "query": query,
-    #     "result": [url_d for url_d in Site.objects.mongo_find({
-    #         'url': {
-    #             '$in': [r[0] for r in result]
-    #         }
-    #     }, {'url': 1, 'tittle': 1, 'description': 1, '_id': 0})]
-    # })
+    query = request.query_params.get('query')
+    limit = request.query_params.get('limit', 15) % 50
+    result = aggregator.search(query)[:limit]
+
+    return Response({
+        "query": query,
+        "result": [url_d for url_d in Site.objects.mongo_find({
+            'url': {
+                '$in': [r[0] for r in result]
+            }
+        }, {'url': 1, 'tittle': 1, 'description': 1, '_id': 0})]
+    })
